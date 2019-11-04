1e2fd030ebb5e9ffd64086b279c4526b1f3fd269
Artem <anandakirtan@mail.ru>
2019-11-04 18:33:46 +0300

    API

--- back/django/app/task/models.py
+++ back/django/app/task/models.py
@@ -6,8 +6,8 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     deadline = models.DateField(auto_now=False, help_text="set a deadline")
-    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    folder = models.ForeignKey(Folder, default = 2, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- back/django/app/task/views.py
+++ back/django/app/task/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets, mixins
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class TaskSelfViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+  def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+  def get_queryset(self):
+    queryset = super(TaskSelfViewSet, self).get_queryset()
+    return queryset.filter(owner=self.request.user)
